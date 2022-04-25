17cf3d3937077dcaf052c9d2942d780b552cabc6
Jonathan Deng <1149978644@qq.com>
2022-04-25 21:37:14 +0800

    Change comparator in MaxArrayDeque to public

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,7 +3,7 @@ package deque;
 import java.util.Comparator;
 
 class MaxArrayDeque<item> extends ArrayDeque<item> {
-    private Comparator<item> comparator;
+    public Comparator<item> comparator;
 
     public MaxArrayDeque(Comparator<item> c){
         comparator = c;
@@ -27,28 +27,9 @@ class MaxArrayDeque<item> extends ArrayDeque<item> {
         return maxItem;
     }
 
-//     public class MaxElement implements Comparator<item>{
-//        public int compare(item item1, item item2){
-//            if (item1 instanceof Integer){
-//                return ((Integer) item1).compareTo(((Integer) item2));
-//            }else if (item1 instanceof String){
-//                return ((String) item1).compareTo(((String) item2));
-//            }else if (item1 instanceof Double){
-//                return ((Double) item1).compareTo(((Double) item2));
-//            }else if (item1 instanceof Float) {
-//                return ((Float) item1).compareTo(((Float) item2));
-//            }else if (item1 instanceof Boolean){
-//                return ((Boolean) item1).compareTo(((Boolean) item2));
-//            }else if(item1 instanceof Long){
-//                return ((Long) item1).compareTo(((Long) item2));
-//            }else {
-//                return ((Short) item1).compareTo(((Short) item2));
-//            }
-//        }
-
-         public class MaxElement<item extends Comparable<item>> implements Comparator<item>{
-             public int compare(item item1, item item2){
-                 return item1.compareTo(item2);
-             }
-     }
+    public class MaxElement<item extends Comparable<item>> implements Comparator<item>{
+         public int compare(item item1, item item2){
+             return item1.compareTo(item2);
+         }
+    }
 }
